6f6d07b1c901a8cfca49bb17e26e4980194e894e
Curtis Poirrier <curtis.poirrier@gmail.com>
2022-08-25 23:05:02 -0500

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongoose').Types.ObjectId
+const url = "uri";
 const uri = 'mongodb+srv://thatcrazysomebeach:Swepea@cluster0.htnm7ae.mongodb.net/?retryWrites=true&w=majority';
 let db = null;
 let users = null;
