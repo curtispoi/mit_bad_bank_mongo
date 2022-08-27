7a66091b0fc34775812f43f1535a545365d32636
Curtis Poirrier <curtis.poirrier@gmail.com>
2022-08-26 23:27:59 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ function Spa() {
         <UserContext.Provider value={{currentUser, setCurrentUser}}>
         <div className="container" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
-          <Route path="/createAccount/" component={CreateAccount} />
+          <Route path="/createaccount/" component={CreateAccount} />
           <Route path="/login/" component={Login} />
           <Route path="/deposit/" component={Deposit} />
           <Route path="/withdraw/" component={Withdraw} />
