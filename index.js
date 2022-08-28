8e0ac46aa8d04ec70b48897cd54a2f60763e413f
Curtis Poirrier <curtis.poirrier@gmail.com>
2022-08-28 15:07:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 const cors = require('cors');
 const dal = require('./dal.js');
 const mongoose = require('mongoose);
-const url = uri;
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
